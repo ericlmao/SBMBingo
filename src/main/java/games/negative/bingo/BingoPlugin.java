44534d3207aa77e2aef21cc6f7927651ab52539b
Negative <negggative@gmail.com>
2023-03-29 16:13:23 -0300

    Update BingoPlugin.java

--- src/main/java/games/negative/bingo/BingoPlugin.java
+++ src/main/java/games/negative/bingo/BingoPlugin.java
@@ -1,11 +1,47 @@
 package games.negative.bingo;
 
+import games.negative.bingo.api.BingoAPI;
+import games.negative.bingo.api.model.team.BingoTeam;
+import games.negative.bingo.core.provider.BingoAPIProvider;
 import games.negative.framework.BasePlugin;
+import org.bukkit.scoreboard.Team;
+
+import java.util.Collection;
 
 public class BingoPlugin extends BasePlugin {
+
+    private BingoAPI api;
 
     @Override
     public void onEnable() {
         super.onEnable();
+
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        reloadConfig();
+
+        this.api = new BingoAPIProvider(this, getConfig());
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+        Collection<BingoTeam> teams = api.getTeamManager().getTeams();
+        for (BingoTeam team : teams) {
+            Team minecraftTeam = team.getMinecraftTeam();
+            if (minecraftTeam == null)
+                continue;
+
+            try {
+                minecraftTeam.unregister();
+            } catch (IllegalStateException e) {
+                // Ignore
+                getLogger().warning("Team for BingoTeam " + team.getBingoColor().toString() + " was already unregistered!");
+                return;
+            }
+        }
+
+        api.getTeamManager().onDisable();
     }
 }

8ecb07c0a773664fbe416fcb6d737be7f3a960c8
Negative <negggative@gmail.com>
2023-03-29 14:52:43 -0300

    Update BingoGoal.java

--- src/main/java/games/negative/bingo/api/model/goal/BingoGoal.java
+++ src/main/java/games/negative/bingo/api/model/goal/BingoGoal.java
@@ -1,11 +1,43 @@
 package games.negative.bingo.api.model.goal;
 
 import games.negative.framework.key.Keyd;
+import org.bukkit.configuration.ConfigurationSection;
+import org.jetbrains.annotations.NotNull;
 
-public interface BingoGoal extends Keyd<String> {
+public abstract class BingoGoal implements Keyd<String> {
 
-    BingoGoalType getType();
+    private final String key;
+    private final BingoGoalType type;
+    private final int amount;
 
-    int getAmount();
-    
+    public BingoGoal(ConfigurationSection section) {
+        this.key = section.getName();
+        this.type = BingoGoalType.valueOf(section.getString("type"));
+        this.amount = section.getInt("amount");
+    }
+
+    public BingoGoal(String key, BingoGoalType type, int amount) {
+        this.key = key;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    @NotNull
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    public BingoGoalType getType() {
+        return type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setKey(@NotNull String s) {
+        throw new UnsupportedOperationException("Cannot set key of BingoGoal");
+    }
 }

846c3a739db700a7e14d915db5bc92298b6c4f96
Negative <negggative@gmail.com>
2023-03-29 15:18:34 -0300

    Add implementation class for BingoTeam

---
+++ src/main/java/games/negative/bingo/core/structure/BingoTeamImpl.java
@@ -0,0 +1,84 @@
+package games.negative.bingo.core.structure;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import games.negative.bingo.api.model.goal.BingoGoal;
+import games.negative.bingo.api.model.team.BingoColor;
+import games.negative.bingo.api.model.team.BingoTeam;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class BingoTeamImpl implements BingoTeam {
+
+    private final BingoColor color;
+    private final Map<BingoGoal, Integer> progress;
+    private final List<UUID> members;
+
+    public BingoTeamImpl(BingoColor color) {
+        this.color = color;
+        this.members = Lists.newArrayList();
+        this.progress = Maps.newHashMap();
+    }
+
+    @Override
+    public Collection<UUID> getMembers() {
+        return members;
+    }
+
+    @Override
+    public void addMember(UUID uuid) {
+        members.add(uuid);
+    }
+
+    @Override
+    public void removeMember(UUID uuid) {
+        members.remove(uuid);
+    }
+
+    @Override
+    public BingoColor getBingoColor() {
+        return color;
+    }
+
+    @Override
+    public Map<BingoGoal, Integer> getProgress() {
+        return progress;
+    }
+
+    @Override
+    public void addProgress(BingoGoal goal, int amount) {
+        if (progress.containsKey(goal)) {
+            progress.put(goal, progress.get(goal) + amount);
+        } else {
+            progress.put(goal, amount);
+        }
+    }
+
+    @Override
+    public void setProgress(BingoGoal goal, int amount) {
+        if (progress.containsKey(goal)) {
+            progress.put(goal, amount);
+        } else {
+            progress.put(goal, amount);
+        }
+    }
+
+    @Override
+    public int getProgress(BingoGoal goal) {
+        return progress.getOrDefault(goal, 0);
+    }
+
+    @Override
+    public @NotNull BingoColor getKey() {
+        return color;
+    }
+
+    @Override
+    public void setKey(@NotNull BingoColor bingoColor) {
+
+    }
+}

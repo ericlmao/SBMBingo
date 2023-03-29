9f3eb951133f2e1d38748361a284f80a5bed5e88
Negative <negggative@gmail.com>
2023-03-29 16:50:41 -0300

    Create BingoTeamCompleteGoalEvent.java

---
+++ src/main/java/games/negative/bingo/api/event/BingoTeamCompleteGoalEvent.java
@@ -0,0 +1,19 @@
+package games.negative.bingo.api.event;
+
+import games.negative.bingo.api.model.goal.BingoGoal;
+import games.negative.bingo.api.model.team.BingoTeam;
+import org.jetbrains.annotations.NotNull;
+
+public class BingoTeamCompleteGoalEvent extends BingoTeamEvent {
+
+    private final BingoGoal goal;
+    public BingoTeamCompleteGoalEvent(@NotNull BingoTeam team, @NotNull BingoGoal goal) {
+        super(team);
+        this.goal = goal;
+    }
+
+    @NotNull
+    public BingoGoal getGoal() {
+        return goal;
+    }
+}

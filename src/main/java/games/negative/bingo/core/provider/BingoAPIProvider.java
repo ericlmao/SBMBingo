638f2a50ee90bf79b10c48d98d9a745f6388f94a
Negative <negggative@gmail.com>
2023-03-29 15:45:03 -0300

    Update BingoAPIProvider.java

--- src/main/java/games/negative/bingo/core/provider/BingoAPIProvider.java
+++ src/main/java/games/negative/bingo/core/provider/BingoAPIProvider.java
@@ -3,13 +3,14 @@ package games.negative.bingo.core.provider;
 import games.negative.bingo.api.BingoAPI;
 import games.negative.bingo.api.BingoTeamManager;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class BingoAPIProvider extends BingoAPI {
 
     private final BingoTeamManager teamManager;
 
-    public BingoAPIProvider(FileConfiguration config) {
-        teamManager = new BingoTeamManagerProvider(config);
+    public BingoAPIProvider(JavaPlugin plugin, FileConfiguration config) {
+        teamManager = new BingoTeamManagerProvider(plugin, config);
     }
 
     @Override

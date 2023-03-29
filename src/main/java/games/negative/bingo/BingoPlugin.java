d6f8ba12d071be315184bfe0b5cab7170951808c
Negative <negggative@gmail.com>
2023-03-29 13:33:21 -0300

    Update plugin credentials

---
+++ src/main/java/games/negative/bingo/BingoPlugin.java
@@ -0,0 +1,11 @@
+package games.negative.bingo;
+
+import games.negative.framework.BasePlugin;
+
+public class BingoPlugin extends BasePlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}

e97b048817e36ad824fe543cfb83d212b509f007
Negative <negggative@gmail.com>
2023-03-29 15:36:23 -0300

    Update BingoTeamManager.java

--- src/main/java/games/negative/bingo/api/BingoTeamManager.java
+++ src/main/java/games/negative/bingo/api/BingoTeamManager.java
@@ -1,4 +1,32 @@
 package games.negative.bingo.api;
 
+import games.negative.bingo.api.model.team.BingoColor;
+import games.negative.bingo.api.model.team.BingoTeam;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+
 public interface BingoTeamManager {
+
+    Collection<BingoTeam> getTeams();
+
+    void addTeam(BingoTeam team);
+
+    void removeTeam(BingoTeam team);
+
+    default BingoTeam getTeam(BingoColor color) {
+        return getTeams().stream().filter(team -> team.getKey().equals(color)).findFirst().orElse(null);
+    }
+
+    Map<UUID, BingoTeam> getUserTeams();
+
+    void addUserTeam(UUID uuid, BingoTeam team);
+
+    void removeUserTeam(UUID uuid);
+
+    default BingoTeam getUserTeam(UUID uuid) {
+        return getUserTeams().get(uuid);
+    }
+
 }

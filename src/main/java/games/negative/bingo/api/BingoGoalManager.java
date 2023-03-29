3d1b6d565f2cb0f43a3351a333665171eefdc4eb
Negative <negggative@gmail.com>
2023-03-29 17:17:43 -0300

    Create BingoGoalManager.java

---
+++ src/main/java/games/negative/bingo/api/BingoGoalManager.java
@@ -0,0 +1,18 @@
+package games.negative.bingo.api;
+
+import games.negative.bingo.api.model.goal.BingoGoal;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.Collection;
+
+public interface BingoGoalManager {
+
+    Collection<BingoGoal> getBingoGoals();
+
+    void addBingoGoal(BingoGoal goal);
+
+    void removeBingoGoal(BingoGoal goal);
+
+    void onReload(FileConfiguration config);
+
+}

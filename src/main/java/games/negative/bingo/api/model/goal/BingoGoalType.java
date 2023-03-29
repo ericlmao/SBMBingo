609f8a374e1f5225bcf51d43b6d0ab9600f1d355
Negative <negggative@gmail.com>
2023-03-29 14:15:51 -0300

    Create BingoGoalType.java

---
+++ src/main/java/games/negative/bingo/api/model/goal/BingoGoalType.java
@@ -0,0 +1,20 @@
+package games.negative.bingo.api.model.goal;
+
+public enum BingoGoalType {
+
+    // Collect Item (pickup item / mine / craft / smelt)
+    COLLECT,
+
+    // Kill Entity (kill entity / kill player)
+    KILL,
+
+    // Get Potion Effect (get potion effect)
+    POTION_EFFECT,
+
+    // Get Advancement (get advancement)
+    ADVANCEMENT,
+
+    // Die (die)
+    DIE,
+
+}

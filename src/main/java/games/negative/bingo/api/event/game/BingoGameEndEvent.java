814f37cc9f7c06b46ef09c8a58a099bf52f70eb8
Negative <negggative@gmail.com>
2023-03-30 08:04:17 -0300

    Update BingoGameEndEvent.java

--- src/main/java/games/negative/bingo/api/event/game/BingoGameEndEvent.java
+++ src/main/java/games/negative/bingo/api/event/game/BingoGameEndEvent.java
@@ -5,7 +5,40 @@ import org.jetbrains.annotations.NotNull;
 
 public class BingoGameEndEvent extends BingoGameEvent {
 
-    public BingoGameEndEvent(@NotNull BingoGameManager gameManager) {
+    private Cause cause;
+    public BingoGameEndEvent(@NotNull BingoGameManager gameManager, @NotNull Cause cause) {
         super(gameManager);
+        this.cause = cause;
+    }
+
+    @NotNull
+    public Cause getCause() {
+        return cause;
+    }
+
+    public void setCause(@NotNull Cause cause) {
+        this.cause = cause;
+    }
+
+    public enum Cause {
+        /**
+         * The game was won by a team
+         */
+        WIN,
+
+        /**
+         * The game has run out of time
+         */
+        TIMEOUT,
+
+        /**
+         * The game was cancelled by some other means
+         */
+        CANCELLED,
+
+        /**
+         * The game was ended by a plugin
+         */
+        UNKNOWN,
     }
 }

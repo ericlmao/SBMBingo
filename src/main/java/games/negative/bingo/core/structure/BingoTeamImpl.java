85bdec77948a4798bdd7f78b810fa3feb9f7aaba
Negative <negggative@gmail.com>
2023-03-30 20:07:49 -0300

    Update BingoTeamImpl.java

--- src/main/java/games/negative/bingo/core/structure/BingoTeamImpl.java
+++ src/main/java/games/negative/bingo/core/structure/BingoTeamImpl.java
@@ -59,7 +59,6 @@ public class BingoTeamImpl implements BingoTeam {
 
     @Override
     public void removeMember(UUID uuid) {
-        team.removeEntry(Bukkit.getOfflinePlayer(uuid).getName());
         members.remove(uuid);
     }
 

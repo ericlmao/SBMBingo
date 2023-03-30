da73244956a49f5c4546843f5d41899a0cc29ea3
Negative <negggative@gmail.com>
2023-03-30 08:16:56 -0300

    Add BingoGameTask runnable

---
+++ src/main/java/games/negative/bingo/task/BingoGameTask.java
@@ -0,0 +1,25 @@
+package games.negative.bingo.task;
+
+import games.negative.bingo.api.event.game.BingoGameEndEvent;
+import games.negative.framework.event.Events;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class BingoGameTask extends BukkitRunnable {
+
+    private final long limit;
+    private final long started;
+
+    public BingoGameTask(long limit) {
+        this.limit = limit;
+        this.started = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+        if (System.currentTimeMillis() < (started + limit))
+            return;
+
+        BingoGameEndEvent event = new BingoGameEndEvent(BingoGameEndEvent.Cause.TIMEOUT);
+        Events.call(event);
+    }
+}

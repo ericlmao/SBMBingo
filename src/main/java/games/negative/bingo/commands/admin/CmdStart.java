1a7f15fd5102fb011bc1cb556494f9689eaed3eb
Negative <negggative@gmail.com>
2023-03-31 05:40:05 -0300

    Update CmdStart.java

--- src/main/java/games/negative/bingo/commands/admin/CmdStart.java
+++ src/main/java/games/negative/bingo/commands/admin/CmdStart.java
@@ -1,4 +1,15 @@
 package games.negative.bingo.commands.admin;
 
-public class CmdStart {
+import games.negative.framework.command.SubCommand;
+import games.negative.framework.command.annotation.CommandInfo;
+import org.bukkit.command.CommandSender;
+
+@CommandInfo(
+        name = "start"
+)
+public class CmdStart extends SubCommand {
+    @Override
+    public void onCommand(CommandSender sender, String[] args) {
+
+    }
 }

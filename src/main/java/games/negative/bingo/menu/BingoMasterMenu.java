882dc097977e2ad475bbb79be3093d1b5f27b779
Negative <negggative@gmail.com>
2023-04-07 12:54:42 -0300

    Change filler menu order in Master Menu

--- src/main/java/games/negative/bingo/menu/BingoMasterMenu.java
+++ src/main/java/games/negative/bingo/menu/BingoMasterMenu.java
@@ -24,11 +24,12 @@ public class BingoMasterMenu extends GUI {
         super("Bingo Master Menu", 5);
 
         List<Integer> fillers = List.of(
-                1, 3, 5, 7,
-                9, 10, 11, 12, 13, 14, 15, 16, 17,
-                19, 21, 23, 25,
-                27, 28, 29, 30, 31, 32, 33, 34, 35,
-                37, 39, 41, 43
+                0, 1, 2, 3, 4, 5, 6, 7, 8,
+                9, 17,
+                18, 19, 20, 21, 22, 23, 24, 25, 26,
+                27, 35,
+                36, 37, 38, 39, 40, 41, 42, 43, 44,
+                45, 46, 47, 49, 51, 52, 53
         );
 
         ItemStack filler = ItemBuilder.newItemBuilder(Material.BLACK_STAINED_GLASS_PANE).setName(" ").build();
